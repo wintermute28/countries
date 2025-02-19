9303c8739d9869cac9f40653349b1619398354cb
wintermute28 <a28c@yandex.ru>
2025-02-19 17:44:17 +0300

    Add Button

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,8 +1,33 @@
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { IoArrowBack } from "react-icons/io5";
+import { searchByCountry } from "../config";
+import Button from "../components/Button";
 
 const Details = () => {
   const { name } = useParams();
-  return <div>Details {name}</div>;
+  const navigate = useNavigate();
+  const [country, setCountry] = useState(null);
+
+  console.log(country);
+
+  useEffect(() => {
+    axios.get(searchByCountry(name)).then(({ data }) => setCountry(data[0]));
+  }, [name]);
+
+  const handleGoBack = () => {
+    navigate(-1);
+  };
+
+  return (
+    <div>
+      <Button onClick={handleGoBack}>
+        <IoArrowBack /> Back
+      </Button>
+      {name}
+    </div>
+  );
 };
 
 export default Details;
